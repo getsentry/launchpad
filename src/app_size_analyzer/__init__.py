dca140b60981b36d866f8a3439aaf5d60c009369
Trevor Elkins <trevor@emergetools.com>
2025-06-09 13:38:19 -0400

    update

--- src/app_size_analyzer/__init__.py
+++ src/app_size_analyzer/__init__.py
@@ -1,14 +1,12 @@
 """App Size Analyzer - CLI tool for analyzing iOS and Android app bundle sizes."""
 
-__version__ = "1.0.0"
-__author__ = "Sentry Team"
-__email__ = "engineering@sentry.io"
+__version__ = "0.0.1"
 
 from .models.results import AnalysisResults, AppInfo, FileAnalysis, BinaryAnalysis
 
 __all__ = [
     "AnalysisResults",
-    "AppInfo", 
+    "AppInfo",
     "FileAnalysis",
     "BinaryAnalysis",
 ]
