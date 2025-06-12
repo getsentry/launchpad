0ee3b26edc68c25b62fedc6ffe281a05019113f6
Nico Hinderling <NicoHinderling@users.noreply.github.com>
2025-06-12 13:14:07 -0700

    fix devenv

--- devenv/sync.py
+++ devenv/sync.py
@@ -3,7 +3,7 @@ from devenv.lib import config, venv
 def main(context: dict[str, str]) -> int:
     reporoot = context["reporoot"]
 
-    venv_dir, python_version, requirements, editable_paths, bins = venv.get(reporoot, "venv")
+    venv_dir, python_version, requirements, editable_paths, bins = venv.get(reporoot, "launchpad")
     url, sha256 = config.get_python(reporoot, python_version)
     print(f"ensuring venv at {venv_dir}...")
     venv.ensure(venv_dir, python_version, url, sha256)
