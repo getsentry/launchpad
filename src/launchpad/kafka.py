7e0d827b98ca274ff101b2806888032beb02478e
Nico Hinderling <NicoHinderling@users.noreply.github.com>
2025-07-23 11:25:52 -0700

    include arroyo_strict_offset_reset in kafka config

--- src/launchpad/kafka.py
+++ src/launchpad/kafka.py
@@ -44,6 +44,7 @@ def create_kafka_consumer(
         "bootstrap.servers": config["bootstrap_servers"],
         "group.id": config["group_id"],
         "auto.offset.reset": config["auto_offset_reset"],
+        "arroyo.strict.offset.reset": config["arroyo_strict_offset_reset"],
         "enable.auto.commit": False,
         "enable.auto.offset.store": False,
     }
@@ -137,4 +138,5 @@ def get_kafka_config() -> Dict[str, Any]:
         "max_pending_futures": int(os.getenv("KAFKA_MAX_PENDING_FUTURES", "100")),
         "healthcheck_file": os.getenv("KAFKA_HEALTHCHECK_FILE"),
         "auto_offset_reset": os.getenv("KAFKA_AUTO_OFFSET_RESET", "latest"),  # latest = skip old messages
+        "arroyo_strict_offset_reset": os.getenv("ARROYO_STRICT_OFFSET_RESET"),
     }
