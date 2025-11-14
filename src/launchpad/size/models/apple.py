377c98737978a0ef91a4c5222360bb4583ca9915
Trevor Elkins <trevor@emergetools.com>
2025-11-13 19:46:09 -0500

    fix

--- src/launchpad/size/models/apple.py
+++ src/launchpad/size/models/apple.py
@@ -115,7 +115,7 @@ class SectionInfo:
     """Extracted section information from LIEF data."""
 
     name: str
-    size: int  # Virtual memory size (includes zero-fill)
+    size: int
     is_zerofill: bool  # True if this section doesn't occupy file space (e.g., __bss)
 
 
